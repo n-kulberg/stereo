00470cd9694f436a46edc570888e856592aed262
Nicholas Kulberg <kulberg@yandex.ru>
2021-09-21 12:00:27 +0300

    corrections in error control tools

--- sources/Epsilon.h
+++ sources/Epsilon.h
@@ -22,21 +22,21 @@ namespace kns_test
 {
 
 
-//! \brief Absolute computation error allowed (factor to std::numeric_limits<T>::epsilon())
-constexpr double	absolute_error_factor(){ return 1; }
+//! \brief Absolute computation error allowed (factor to std::numeric_limits<T>::denorm_min())
+constexpr double	absolute_error_factor(){ return 1000; }
 
 //! \brief Relative computation error allowed (factor to std::numeric_limits<T>::epsilon()
-constexpr double	relative_error_factor(){ return 10; }
+constexpr double	relative_error_factor(){ return 100; }
 
 
 
 //! \brief Absolute computation error allowed. Determine if the value x is negligibly small
 template<class T>
-inline bool is_almost_zero(T x) { return std::abs(x) < absolute_error_factor()*std::numeric_limits<T>::epsilon(); }
+inline bool is_almost_zero(T x) { return std::abs(x) <= absolute_error_factor()*std::numeric_limits<T>::denorm_min(); } // strict < is incorrect, <= is needed
 
 //! \brief Determine if value x is negligible to y. Notation inspired by https://en.wikipedia.org/wiki/Big_O_notation
 template<class T>
-inline bool is_small_o(T x, T y){ return std::abs(x) < std::abs(y)* relative_error_factor()*std::numeric_limits<T>::epsilon(); }
+inline bool is_small_o(T x, T y){ return std::abs(x) <= std::abs(y)* relative_error_factor()*std::numeric_limits<T>::epsilon(); } // strict < is incorrect, <= is needed
 
 }//namespace kns_test
 
