b989d75a70f00d8371c0d677c9a02b3accd88b06
Nicholas Kulberg <kulberg@yandex.ru>
2021-09-22 13:24:43 +0300

    tests refactoring

--- sources/SectionsDistanceTest.cpp
+++ sources/SectionsDistanceTest.cpp
@@ -82,8 +82,8 @@ pair<s3_F64, s3_F64> RandomSkewingSections(double z, bool intersect)
 
 	auto	rm = RandomRotationMatrix();
 
-	s1 = matrix_multiply(rm, s1);
-	s2 = matrix_multiply(rm, s2);
+// 	s1 = matrix_multiply(rm, s1);
+// 	s2 = matrix_multiply(rm, s2);
 
 	return make_pair(s1, s2);
 }
@@ -93,25 +93,33 @@ pair<s3_F64, s3_F64> RandomSkewingSections(double z, bool intersect)
 using namespace kns_test;
 using namespace std;
 
-void test_skewing(double z, bool intersect_xy)// testing skew sections that may intersect in XY plane or not
+void test_skewing(double param, bool intersect_xy)// testing skew sections that may intersect in XY plane or not
 {
- 		auto	sp = RandomSkewingSections(z, intersect_xy); //passed OK
+ 		auto	sp = RandomSkewingSections(param, intersect_xy); //passed OK
+
 		double	dist = ComputeDistance(sp.first, sp.second);
-		cout << endl << "\tpre-defined z param=" << z << "\testimated distance=" << dist;
+		cout << endl << "\tpre-defined param=" << param << "\testimated distance=" << dist;
+		if(dist != param)
+		{
+			cout << "err";
+			ComputeDistance(sp.first, sp.second);
+		}
 }
 
-void test_colineary(double z)// testing colineary sections
+void test_colineary(double param)// testing colineary sections
 {
-	auto	sp = RandomColinearySections(z); //Estimated distance must max(z-1, 0). passed OK
+	auto	sp = RandomColinearySections(param); //Estimated distance must max(z-1, 0). passed OK
 	double	dist = ComputeDistance(sp.first, sp.second);
-	cout << endl << "\tpre-defined z param=" << z << "\testimated distance=" << dist << "\ttrue distance = max(z-1,0)=" << std::max(z-1., 0.);
+	double true_distance = std::max(param-1., 0.);
+	cout << endl << "\tpre-defined param=" << param << "\testimated distance=" << dist << "\ttrue distance =" << true_distance;
 }
 
-void test_parallel(double y, double z)// testing parallel sections
+void test_parallel(double y, double param)// testing parallel sections
 {
-	auto	sp = RandomParallelSections(y, z); //Estimated distance must hypot(1, max(z-1, 0)). passed OK
+	auto	sp = RandomParallelSections(y, param); //Estimated distance must hypot(1, max(z-1, 0)). passed OK
 	double	dist = ComputeDistance(sp.first, sp.second);
-	cout << endl << "\testimated distance=" << dist << "\ttrue distance = hypot(y,max(z-1,0))=" << hypot(y, std::max(z-1., 0.)) << endl;
+	double true_distance = hypot(y, std::max(param-1., 0.));
+	cout << endl << "\testimated distance=" << dist << "\ttrue distance = " << true_distance << endl;
 }
 
 
@@ -119,17 +127,17 @@ void	TestSegment3DistanceAuto()
 {
 	clock_t	t = clock();
 	srand(t);
-	size_t	N = 10;
+	size_t	N = 20;
 	for(size_t i = 0; i < N; ++i)
 	{
 		double	z = i*0.25;
 		double y = 1;
 
-		cout << "Test #" << i << ".";
+		cout << "\nTest #" << i << ".";
 		test_skewing(z, true);
-		test_skewing(z, false);
-		test_colineary(z);
-		test_parallel(y,z);
+// 		test_skewing(z, false);
+// 		test_colineary(z);
+// 		test_parallel(y,z);
 
 	}
 }
